d78cb51adbda1276402473a6f8de5fd944c395c9
Thomas Arndt <arndtt42@gmail.com>
2022-03-10 18:33:34 -0800

    Commit of code including config, index, autoload, model, and view files

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+// site name
+define('SITE_NAME', 'my-first-PHP-MVC');
+
+// App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
+// DB Params
+define('DB_HOST', 'http://localhost:3306');
+define('DB_USER', 'arndtt');
+define('DB_PASS', 'rootroot');
+define('DB_NAME', 'first_php_db_schema');
---
+++ public/index.php
@@ -0,0 +1,3 @@
+// Load Config
+require_once '../config/config.php';
+require_once '../vendor/autoload.php';
